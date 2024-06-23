1052bf8bad7361a84eb637f4eed046b045ec290d
030820acc <030820acc@gmail.com>
2024-06-22 22:46:02 -0400

    getting the foundation classes written

---
+++ cell.py
@@ -0,0 +1,42 @@
+from line import Line
+from point import Point
+
+class Cell:
+    def __init__(self, x1, x2, y1, y2, window, top=True, bottom=True, left=True, right=True):
+        self.has_left_wall = left
+        self.has_right_wall = right
+        self.has_top_wall = top
+        self.has_bottom_wall = bottom
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._top_left = Point(x1, y1)
+        self._top_right = Point(x2, y1)
+        self._bottom_right = Point(x2, y2)
+        self._bottom_left = Point(x1, y2)
+        self._top = Line(self._top_left, self._top_right)
+        self._bottom = Line(self._bottom_left, self._bottom_right)
+        self._right = Line(self._top_right, self._bottom_right)
+        self._left = Line(self._top_left, self._bottom_left)
+        self._window = window
+
+    def draw(self):
+        canvas = self._window.canvas
+        if self.has_top_wall != False:
+            self._window.draw_line(self._top, "black")
+        if self.has_bottom_wall:
+            self._window.draw_line(self._bottom, "black")
+        if self.has_left_wall:
+            self._window.draw_line(self._left, "black")
+        if self.has_right_wall:
+            self._window.draw_line(self._right, 'black')
+
+    def move_draw(self, to_cell, undo=False):
+        self_center = Point((self._x2 + self._x1) / 2, (self._y2 + self._y1) / 2)
+        other_center = Point((to_cell._x2 + to_cell._x1) / 2, (to_cell._y2 + to_cell._y1) / 2)
+        new_line = Line(self_center, other_center)
+        if undo:
+            self._window.draw_line(new_line, "red")
+        else:
+            self._window.draw_line(new_line, "black")
--- main.py
+++ main.py
@@ -1,60 +1,15 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window():
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root_widget = Tk()
-        self.root_widget.title = "Maze Solver"
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
-        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
-
-    def draw_line(self, line, color):
-        line.draw(self.canvas, color)
-
-    def redraw(self):
-        self.root_widget.update()
-        self.root_widget.update_idletasks()
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running == True:
-            self.redraw()
-
-    def close(self):
-        self.running = False
-
-
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-class Line():
-    def __init__(self, point_one, point_two):
-        self.point_one = point_one
-        self.point_two = point_two
-
-    def draw(self, canvas, color):
-        canvas.create_line(
-            self.point_one.x, 
-            self.point_one.y,
-            self.point_two.x,
-            self.point_two.y,
-            fill=color,
-            width=2
-        )
-
+from window import Window
+from cell import Cell
 
 def main():
-    win = Window(800, 600)
-    point_a = Point(50, 50)
-    point_b = Point(100, 100)
-    line_a = Line(point_a, point_b)
-    win.draw_line(line_a, "black")
+    win = Window(1000, 800)
+    cell1 = Cell(20, 70, 100, 150, win, left=False)
+    cell2 = Cell(25, 300, 80, 500, win, top=False)
+    cell3 = Cell(150, 200, 200, 300, win)
+    cell1.draw()
+    cell1.move_draw(cell3, undo=True)
+    cell2.draw()
+    cell3.draw()
     win.wait_for_close()
 
 
